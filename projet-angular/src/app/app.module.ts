5dbd794181e372787d3648840f17bd4a13c1a8d1
ceepee86 <62884636+ceepee86@users.noreply.github.com>
2020-04-07 15:30:33 +0200

    Projet de base Angular

--- projet-angular/src/app/app.module.ts
+++ projet-angular/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { SigninComponent } from './connexion/signin/signin.component';
+import { SignupComponent } from './connexion/signup/signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { EditprofileComponent } from './user/editprofile/editprofile.component';
+import { AnnonceComponent } from './annonce/annonce.component';
+import { ListComponent } from './user/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    SigninComponent,
+    SignupComponent,
+    AdminComponent,
+    UserComponent,
+    ProfileComponent,
+    EditprofileComponent,
+    AnnonceComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
