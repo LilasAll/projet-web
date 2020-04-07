0bb1b10c9da5bf9714dd9e3dffe6c5a3a796ba4b
LilasAll <lilas.allard@gmail.com>
2020-04-07 10:16:25 +0200

    angular created

--- projet-angular/README.md
+++ projet-angular/README.md
@@ -1,4 +1,4 @@
-# ProjetWeb
+# ProjetAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- projet-angular/src/app/app.component.spec.ts
+++ projet-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projet-web'`, () => {
+  it(`should have as title 'projet-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projet-web');
+    expect(app.title).toEqual('projet-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('projet-web app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('projet-angular app is running!');
   });
 });
