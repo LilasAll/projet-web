3390a83a43cee9a2520aa360801f5fe91a01e13c
BenjaminThomaas <b-thomas@outlook.fr>
2020-04-09 23:56:18 +0200

    Création de l'htlm pour les componement profile et editprofile

--- projet-angular/src/app/app-routing.module.ts
+++ projet-angular/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
+import { AnnonceComponent } from './annonce/annonce.component';
+import { EditprofileComponent } from './user/editprofile/editprofile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent }
+  { path: '', component: LayoutComponent },
+  {path: 'editprofil', component: EditprofileComponent},
+  {path: 'profil', component: ProfileComponent},
+  {path:'annonce', component:AnnonceComponent}
 ];
 
 @NgModule({
