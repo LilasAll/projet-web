4960bd5235fb7a3e9146cd7926011432da654036
LilasAll <lilas.allard@gmail.com>
2020-04-14 11:30:55 +0200

    gestion redirection authentification

--- projet-angular/src/app/app-routing.module.ts
+++ projet-angular/src/app/app-routing.module.ts
@@ -1,19 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AnnonceComponent } from './annonce/annonce.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './service/auth/auth.guard';
 
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'annonces', component: AnnonceComponent }
+  { path: 'annonces', component: AnnonceComponent },
+
+  //path for authentification parts :  
+  { path: 'login', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', canActivate: [AuthGuard], component: HomeComponent }
+
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
+
 export class AppRoutingModule { }
--- projet-angular/src/app/login/login.component.ts
+++ projet-angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(private router: Router) {
+   }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log('Tentative de connexion');
+    //permet de verif si les login/mdp sont corrects, par exemple par une requête à un service REST 
+    localStorage.setItem('user', JSON.stringify({login : this.model.pseudo})); 
+    this.router.navigate(['/home']);
   }
 
 }
