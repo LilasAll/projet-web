9843ce51f8e9e760f4c83651518391b1f1c600c5
Antoine <antoinebertinb@gmail.com>
2020-04-15 14:09:03 +0200

    spring MAJ Mapping

--- projet-spring/src/main/java/com/inti/formation/ATeamProjectApplication.java
+++ projet-spring/src/main/java/com/inti/formation/ATeamProjectApplication.java
@@ -2,18 +2,16 @@ package com.inti.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class ATeamProjectApplication {
+	
+
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ATeamProjectApplication.class, args);
 	}
+
 
 }
