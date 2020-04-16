4b010591fe633bcbd3185e6fd6e74fac980fb3ee
BenjaminThomaas <b-thomas@outlook.fr>
2020-04-16 18:00:52 +0200

    Ajout du update dans editprofil

--- projet-angular/src/app/user/editprofile/editprofile.component.ts
+++ projet-angular/src/app/user/editprofile/editprofile.component.ts
@@ -1,6 +1,7 @@
-import { Router } from '@angular/router';
+import { Router, Params, ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editprofile',
@@ -9,8 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditprofileComponent implements OnInit {
   id: any;
-  constructor(private userService: UserService, private router: Router) { }
+  form: FormGroup;
+  index:any;
+  mode: boolean;
+  constructor(private userService: UserService,private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      userId : new FormControl(null),
+      pseudo : new FormControl(null,Validators.required),
+      email : new FormControl(null,Validators.required),
+      password : new FormControl(null,Validators.required),
+      postalCodeUser : new FormControl(null, Validators.required),
+      phone : new FormControl(null, Validators.required),
+    })
+
+    this.activatedRoute.params.subscribe ((param: Params) => {
+      this.index = param['index'];
+
+      if(this.index) {
+        this.form.setValue(this.userService.users [this.index]);
+      }
+    })
   }
+
+  updateUser() {
+    this.userService.update(this.form.value).subscribe(response =>{
+    this.userService.users.push(response.body);
+    console.log(this.userService.users);
+    this.form.reset();
+  });
+}
+
+
+
 }
